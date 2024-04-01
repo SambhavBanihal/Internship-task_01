101f9327b318a9d0fc02e5fa33e18d60e4504bc7
SambhavBanihal <148674667+SambhavBanihal@users.noreply.github.com>
2024-04-01 22:28:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const hamburger = document.getElementById("hamburger")
+
+const navlist = document.getElementById("navlist")
+
+hamburger.addEventListener("click", () => {
+  navlist.classList.toggle("navlist-active")
+})
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500
+  },
+  breakpoints: {
+    1200: { slidesPerView: 3, },
+    800: { slidesPerView: 2, },
+    300: { slidesPerView: 1, },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
